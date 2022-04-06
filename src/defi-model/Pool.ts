4487034d186fa8f1614f2ee2b2948b9a4a4be354
David Jeong <drumrobot@1do.space>
2022-04-06 15:26:00 +0900

    Add address field to store contract address

--- src/defi-model/Pool.ts
+++ src/defi-model/Pool.ts
@@ -1,16 +1,18 @@
 import { Schema } from 'mongoose';
 import { BaseModel, model } from '.';
 
-export class Pool extends BaseModel<Schema.Types.ObjectId> {
+export class Pool extends BaseModel<String> {
   name: String;
+  address: String;
   liquidity: Number;
   lp_price: Number;
   protocol: String;
   updated_at: Date;
 
   static definition = {
-    _id: Schema.Types.ObjectId,
+    _id: String,
     name: String,
+    address: String,
     liquidity: Number,
     lp_price: Number,
     protocol: {
